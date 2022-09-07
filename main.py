8a7316e078e1f00c72e1d5d5245537510155b1bc
mohammadpagard <mohammadpagard2003@gmail.com>
2022-09-07 19:28:26 +0430

    Complete the file created instance method.

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import os
 
 
 class FileManager:
-	def find(self, name, address):
+	def find(self, address, name):
 		pass
 
-	def create_file(self, name, address):
-		pass
+	def create_file(self, address, name):
+		full_address = os.path.join(address, name)
+		file = open(full_address, "a")
+		file.close()
 
 	def create_folder(self, address, name):
 		full_address = os.path.join(address, name)
 		os.makedirs(full_address)
 
-	def delete(self, name, address):
+	def delete(self, address, name):
 		pass
 
 	def restore(self, name):
